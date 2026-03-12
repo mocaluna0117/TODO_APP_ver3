fc15d4f0f02a2656fd8f7dfbe5cfaa27743b0175
daiki.kimura <daibon20020117@gmail.com>
2026-03-13 03:40:09 +0900

    バックエンドもdockerコンテナ化,devcontainerでbackendを開き開発できるようになった

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,8 +7,11 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
+    // GraphQLModuleはGraphQLサーバーを立てる役割で、NestJSでGraphQLを使うためのモジュール
+    // forRootはGraphQLModuleをアプリ全体で使うという意味、NestJSではよくこの書き方をする
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
+      driver: ApolloDriver, // GraphQLエンジンに Apollo を使う
+      // GraphQLのschemaを自動生成する(Code First（NestJS推奨）)
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     PrismaModule,
@@ -16,3 +19,19 @@ import { TodoModule } from './todo/todo.module';
   ],
 })
 export class AppModule {}
+
+/* 
+Todo.model.ts
+Resolver.ts
+Input.ts
+      ↓
+GraphQLModule
+      ↓
+autoSchemaFile
+      ↓
+schema.gql 自動生成
+      ↓
+Apollo Server
+      ↓
+http://localhost:3050/graphql
+*/
